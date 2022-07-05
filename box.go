23ba8d26879eda08b27d50c9210e98e5d53e0ed7
asavitski <katexizis94@gmail.com>
2022-07-05 16:23:05 +0300

    added solution

--- box.go
+++ box.go
@@ -1,5 +1,8 @@
 package golang_united_school_homework
 
+import (
+	"errors"
+)
 
 // box contains list of shapes and able to perform operations on them
 type box struct {
@@ -17,45 +20,100 @@ func NewBox(shapesCapacity int) *box {
 // AddShape adds shape to the box
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
-	panic("implement me")
+	if len(b.shapes) < b.shapesCapacity {
+		b.shapes = append(b.shapes, shape)
+		return nil
+	}
+
+	return errors.New("max number of shapes")
 }
 
 // GetByIndex allows getting shape by index
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
-	panic("implement me")
+	if i < 0 || i > b.shapesCapacity {
+		return nil, errors.New("index out of max range")
+	}
 
+	if len(b.shapes) < i {
+		return nil, errors.New("index out of current range")
+	}
+
+	return b.shapes[i], nil
 }
 
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
-	panic("implement me")
+	if i < 0 || i > b.shapesCapacity {
+		return nil, errors.New("index out of max range")
+	}
 
+	if len(b.shapes) < i {
+		return nil, errors.New("index out of current range")
+	}
+
+	var shape = b.shapes[i]
+	b.shapes = append(b.shapes[:i], b.shapes[i+1:]...)
+
+	return shape, nil
 }
 
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
-	panic("implement me")
+	if i < 0 || i > b.shapesCapacity {
+		return nil, errors.New("index out of max range")
+	}
 
+	if len(b.shapes) < i {
+		return nil, errors.New("index out of current range")
+	}
+
+	var removedShape = b.shapes[i]
+
+	b.shapes[i] = shape
+
+	return removedShape, nil
 }
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
 func (b *box) SumPerimeter() float64 {
-	panic("implement me")
+	var sum float64
 
+	for _, shape := range b.shapes {
+		sum += shape.CalcPerimeter()
+	}
+
+	return sum
 }
 
 // SumArea provides sum area of all shapes in the list.
 func (b *box) SumArea() float64 {
-	panic("implement me")
+	var sum float64
 
+	for _, shape := range b.shapes {
+		sum += shape.CalcArea()
+	}
+
+	return sum
 }
 
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
-	panic("implement me")
+	var isAnyCircle bool = false
+	for i := 0; i < len(b.shapes); i++ {
+		switch b.shapes[i].(type) {
+		case Circle:
+			b.shapes = append(b.shapes[:i], b.shapes[i+1:]...)
+			isAnyCircle = true
+		}
+	}
 
+	if isAnyCircle {
+		return errors.New("no circles in box")
+	}
+
+	return nil
 }
--- triangle.go
+++ triangle.go
@@ -1,6 +1,16 @@
 package golang_united_school_homework
 
+import "math"
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+func (r Triangle) CalcPerimeter() float64 {
+	return r.Side * 3
+}
+
+func (r Triangle) CalcArea() float64 {
+	return (math.Pow(r.Side, 2) * math.Sqrt(3)) / 4
 }
